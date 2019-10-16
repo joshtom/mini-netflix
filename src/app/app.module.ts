98907ffa253090a0d692bb502ad8e430d5b6ddb0
joshtom <olajidejoshua4real@gmail.com>
2019-10-16 05:19:58 +0100

    generated movie list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
