c6ede70b2cefa23abb4029a139932a3105acc7cf
joshtom <olajidejoshua4real@gmail.com>
2019-10-17 17:19:55 +0100

    Fixed router module not well imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
