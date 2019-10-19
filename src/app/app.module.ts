54ab90189088f0bafd71770ab8d249863c6dfb1a
joshtom <olajidejoshua4real@gmail.com>
2019-10-19 04:28:27 +0100

    Imported httpModule from angular core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
