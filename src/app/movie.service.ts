a57abfab881f956bd444148d005ae1b04204085b
joshtom <olajidejoshua4real@gmail.com>
2019-10-20 01:16:19 +0100

    Added a client http into the constructor

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
